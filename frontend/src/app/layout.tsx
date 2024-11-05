01aa856ca4433038a50ffe4f1ddfe3e598aa09f4
andresbonelli <andresbonellipiano@gmail.com>
2024-11-05 11:55:00 -0300

    Update 18 Font types (San Francisco PRO)

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,55 +3,105 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const sfProRegular = localFont({
-  src: "../../public/fonts/sf-pro-font/SFPRODISPLAYREGULAR.otf",
+  src: "../../public/fonts/sf-pro-font/SF-Pro-Display-Regular.otf",
   variable: "--font-sf-pro-regular",
   weight: "100 900",
 });
 
+const sfProRegularItalic = localFont({
+  src: "../../public/fonts/sf-pro-font/SF-Pro-Display-RegularItalic.otf",
+  variable: "--font-sf-pro-regular-italic",
+  weight: "100 900",
+});
+
 const sfProBold = localFont({
-  src: "../../public/fonts/sf-pro-font/SFPRODISPLAYBOLD.otf",
+  src: "../../public/fonts/sf-pro-font/SF-Pro-Display-Bold.otf",
   variable: "--font-sf-pro-bold",
+  weight: "100 900",
+});
+const sfProBoldItalic = localFont({
+  src: "../../public/fonts/sf-pro-font/SF-Pro-Display-BoldItalic.otf",
+  variable: "--font-sf-pro-bold-italic",
   weight: "100 900",
 });
 
 const sfProMedium = localFont({
-  src: "../../public/fonts/sf-pro-font/SFPRODISPLAYMEDIUM.otf",
+  src: "../../public/fonts/sf-pro-font/SF-Pro-Display-Medium.otf",
   variable: "--font-sf-pro-medium",
   weight: "100 900",
 });
 
+const sfProMediumItalic = localFont({
+  src: "../../public/fonts/sf-pro-font/SF-Pro-Display-MediumItalic.otf",
+  variable: "--font-sf-pro-medium-italic",
+  weight: "100 900",
+});
+
+const sfProSemibold = localFont({
+  src: "../../public/fonts/sf-pro-font/SF-Pro-Display-Semibold.otf",
+  variable: "--font-sf-pro-semibold",
+  weight: "100 900",
+});
+
 const sfProSemiboldItalic = localFont({
-  src: "../../public/fonts/sf-pro-font/SFPRODISPLAYSEMIBOLDITALIC.otf",
+  src: "../../public/fonts/sf-pro-font/SF-Pro-Display-SemiboldItalic.otf",
   variable: "--font-sf-pro-semibold-italic",
   weight: "100 900",
 });
 
+const sfProHeavy = localFont({
+  src: "../../public/fonts/sf-pro-font/SF-Pro-Display-Heavy.otf",
+  variable: "--font-sf-pro-heavy",
+  weight: "100 900",
+});
+
 const sfProHeavyItalic = localFont({
-  src: "../../public/fonts/sf-pro-font/SFPRODISPLAYHEAVYITALIC.otf",
+  src: "../../public/fonts/sf-pro-font/SF-Pro-Display-HeavyItalic.otf",
   variable: "--font-sf-pro-heavy-italic",
   weight: "100 900",
 });
 
+const sfProLight = localFont({
+  src: "../../public/fonts/sf-pro-font/SF-Pro-Display-Light.otf",
+  variable: "--font-sf-pro-light",
+  weight: "100 900",
+});
 const sfProLightItalic = localFont({
-  src: "../../public/fonts/sf-pro-font/SFPRODISPLAYLIGHTITALIC.otf",
+  src: "../../public/fonts/sf-pro-font/SF-Pro-Display-LightItalic.otf",
   variable: "--font-sf-pro-light-italic",
   weight: "100 900",
 });
 
+const sfProBlack = localFont({
+  src: "../../public/fonts/sf-pro-font/SF-Pro-Display-Black.otf",
+  variable: "--font-sf-pro-black",
+  weight: "100 900",
+});
 const sfProBlackItalic = localFont({
-  src: "../../public/fonts/sf-pro-font/SFPRODISPLAYBLACKITALIC.otf",
+  src: "../../public/fonts/sf-pro-font/SF-Pro-Display-BlackItalic.otf",
   variable: "--font-sf-pro-black-italic",
   weight: "100 900",
 });
 
+const sfProThin = localFont({
+  src: "../../public/fonts/sf-pro-font/SF-Pro-Display-Thin.otf",
+  variable: "--font-sf-pro-thin",
+  weight: "100 900",
+});
 const sfProThinItalic = localFont({
-  src: "../../public/fonts/sf-pro-font/SFPRODISPLAYTHINITALIC.otf",
+  src: "../../public/fonts/sf-pro-font/SF-Pro-Display-ThinItalic.otf",
   variable: "--font-sf-pro-thin-italic",
   weight: "100 900",
 });
 
+const sfProUltralight = localFont({
+  src: "../../public/fonts/sf-pro-font/SF-Pro-Display-Ultralight.otf",
+  variable: "--font-sf-pro-ultralight",
+  weight: "100 900",
+});
+
 const sfProUltralightItalic = localFont({
-  src: "../../public/fonts/sf-pro-font/SFPRODISPLAYULTRALIGHTITALIC.otf",
+  src: "../../public/fonts/sf-pro-font/SF-Pro-Display-UltralightItalic.otf",
   variable: "--font-sf-pro-ultralight-italic",
   weight: "100 900",
 });
@@ -71,13 +121,22 @@ export default function RootLayout({
       <body
         className={`
       ${sfProRegular.variable}
+      ${sfProRegularItalic.variable}
+      ${sfProBlack.variable}
       ${sfProBlackItalic.variable}
       ${sfProBold.variable}
+      ${sfProBoldItalic.variable}
+      ${sfProHeavy.variable}
       ${sfProHeavyItalic.variable}
+      ${sfProLight.variable}
       ${sfProLightItalic.variable}
       ${sfProMedium.variable}
+      ${sfProMediumItalic.variable}
+      ${sfProSemibold.variable}
       ${sfProSemiboldItalic.variable}
+      ${sfProThin.variable}
       ${sfProThinItalic.variable}
+      ${sfProUltralight.variable}
       ${sfProUltralightItalic.variable}
       antialiased`}
       >
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,17 +2,26 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-start font-[family-name:var(--font-sf-pro-regular)]">
+    <div className="flex flex-col justify-start font-regular">
       <main className="flex flex-col items-center gap-5">
-        <h1 className="text-5xl">Regular</h1>
-        <h1 className="text-5xl font-bold">Bold</h1>
-        <h1 className="text-5xl font-medium">Medium</h1>
-        <h1 className="text-5xl font-black-italic">Black Italic</h1>
-        <h1 className="text-5xl font-semibold-italic">Semibold Italic</h1>
-        <h1 className="text-5xl font-light-italic">Light Italic</h1>
-        <h1 className="text-5xl font-thin-italic">Thin</h1>
-        <h1 className="text-5xl font-ultralight-italic">Ultralight Italic</h1>
-        <h1 className="text-5xl font-heavy-italic">Heavy Italic</h1>
+        <h1 className="text-3xl font-ultralight">Ultralight</h1>
+        <h1 className="text-3xl font-ultralight-italic">Ultralight Italic</h1>
+        <h1 className="text-3xl font-thin">Thin</h1>
+        <h1 className="text-3xl font-thin-italic">Thin Italic</h1>
+        <h1 className="text-3xl font-light">Light</h1>
+        <h1 className="text-3xl font-light-italic">Light Italic</h1>
+        <h1 className="text-3xl">Regular</h1>
+        <h1 className="text-3xl font-regular-italic">Regular Italic</h1>
+        <h1 className="text-3xl font-medium">Medium</h1>
+        <h1 className="text-3xl font-medium-italic">Medium Italic</h1>
+        <h1 className="text-3xl font-semibold">Semibold</h1>
+        <h1 className="text-3xl font-semibold-italic">Semibold Italic</h1>
+        <h1 className="text-3xl font-bold">Bold</h1>
+        <h1 className="text-3xl font-bold-italic">Bold Italic</h1>
+        <h1 className="text-3xl font-heavy">Heavy</h1>
+        <h1 className="text-3xl font-heavy-italic">Heavy Italic</h1>
+        <h1 className="text-3xl font-black">Black</h1>
+        <h1 className="text-3xl font-black-italic">Black Italic</h1>
       </main>
     </div>
   );
